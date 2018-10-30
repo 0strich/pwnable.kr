39a6e0c64b6a7f2a17a6125723adafd46bd178be
0strich <gee8195@naver.com>
2018-10-30 22:38:53 +0900

    backup version

--- coin2/coin2.py
+++ coin2/coin2.py
@@ -1,23 +1,46 @@
-N, C = 14, 4
-sqaure = 0
+from socket import *
+
 string = ''
-backeup = 0
+square, binary = 0, 0
 sw = 1
+add = 0
+calc_bin = 0
 
-for i in range(C):
-    square = 2 ** i
-    backup = square
-    for j in range(square-1, N):
-        if(sw % 2 == 1):
-            string += str(j+1) + ' '
-            backup -= 1
+addr = ('pwnable.kr', 9008)
+s = socket()
+s.connect(addr)
+
+s.recv(2048)
+
+# Repeat 100 times
+for i in range(100):
+    lst = s.recv(20).split()
+    N, C = int(lst[0][2:]), int(lst[1][2:])
+
+    for i in range(C):
+        square = 2 ** i
+        binary = square
+        for j in range(square,N-1):
+            if(sw % 2 == 1):
+                string += str(j-1) + ' '
+                binary -= 1
+            else:
+                binary -= 1
+
+            if(binary == 0):
+                sw += 1
+                binary = square
+        string = string[:-1] + '-'
+        sw = 1
+    string = string[:-1] + '\n'
+    lst = s.recv(200).split('-')
+    calc_bin = len(lst)
+    for result in range(calc_bin):
+        if(lst[result] % 10 == 0):
+            pass
         else:
-            backup -= 1
+            add += 2 ** (calc_bin - result)
+    s.send(str(add) + '\n')
+    print(s.recv(1024))
 
-        if(backup == 0):
-            sw += 1
-            backup = square
-
-    print(string)
-    string = ''
-    sw = 1
+s.close()
